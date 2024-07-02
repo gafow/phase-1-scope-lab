deff8b6c54ba01ef34155584fd9597daead86ea9
yasir <yasir.abdiweli@student.moringa.com>
2024-07-02 20:37:21 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(customerName){
+    return bestCustomer = customerName;
+}
+
+const leastFavoriteCustomer = 'Andy';
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'Ben';
+}
+
